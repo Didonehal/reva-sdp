d19cde271aae32bd1c19f4167866b3c975bff2b8
Didonehal <20010118323.didonehal@cse.reva.edu.in>
2021-12-21 09:41:29 +0530

    change 5 [compliant with industry standards]

--- reva-sdp/src/main/java/com/cynefian/revasdp/SkillsDevelopmentProgram.java
+++ reva-sdp/src/main/java/com/cynefian/revasdp/SkillsDevelopmentProgram.java
@@ -7,8 +7,7 @@ public class SkillsDevelopmentProgram {
 	static Logger log = Logger.getLogger(SkillsDevelopmentProgram.class.getName());
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to the SDP");
-		System.out.println("Didonehal N Gowda 12345");
+		
 		logMessage();
 	}
 	
